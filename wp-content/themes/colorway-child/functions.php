196c520d4057bfa11912255a4c7d027d2c28145f
Valerie <valeriec@san.rr.com>
2015-04-01 10:41:07 -0700

    v2

---
+++ wp-content/themes/colorway-child/functions.php
@@ -0,0 +1,17 @@
+<?php //Child theme's functions file runs before the parent theme's functions.php
+//
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style') );
+ 
+}
+
+
+
+
+
+//no close 	PHP
